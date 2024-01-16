67683bda779ae5a482f2d1bca112f5dd1396f9c4
Elena Arocena <elena.arocena@innoway.es>
2024-01-16 10:50:51 +0100

    first commit

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'New',
+              icon: 'pi pi-fw pi-plus',
+          },
+          {
+              label: 'Delete',
+              icon: 'pi pi-fw pi-trash'
+          }
+      ];
+  }
+}
